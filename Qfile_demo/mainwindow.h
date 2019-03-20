1b7f3f1b19ddcc0379efe4a3ace216c193fd0f53
Jivansha <35500169+Jivansha@users.noreply.github.com>
2019-03-21 00:22:13 +0530

    Add files via upload

---
+++ Qfile_demo/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<QTimer>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+public slots:
+    void myfunction();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QTimer *timer;
+};
+
+#endif // MAINWINDOW_H
